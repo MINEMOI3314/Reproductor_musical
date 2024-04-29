9df1e3f18088f4fb5b13f2d86b3358f70598cb04
MINEMOI3314 <minemoi197@gmail.com>
2024-04-28 18:42:48 -0600

    h

--- Scrips/metadatos.js
+++ Scrips/metadatos.js
@@ -1,5 +1,35 @@
 console.log("hola mundo")
 
 function prueba_b(){
-    alert ("me haz presionado")
+    console.log ("me haz presionado")
+}
+
+class Metadatos {
+    constructor (nombre, artista, salida){
+        this.nombre = nombre
+        this.artista = artista
+        this.salida = salida
+    }
+    hablar(){
+        return `en ${this.salida} salió la cancion ${this.nombre} del artista ${this.artista}`
+    }
+}
+
+
+var cancion = [
+    new Metadatos("Enter sandman", "Metalica", 1991), 
+    new Metadatos ("Aca entre nos", "Vicente Fernades", 1992), 
+    new Metadatos ("Tu falta de querer", "Mon laferte", 2015)
+]
+
+console.log(cancion[0].hablar())
+
+function bot1(){
+    alert (cancion[0].hablar())
+}
+function bot2(){
+    alert (cancion[1].hablar())
+}
+function bot3(){
+    alert (cancion[2].hablar())
 }
