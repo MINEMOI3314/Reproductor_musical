3332ea3f9550f42ef5fae451af4f42f860e54c90
MINEMOI3314 <minemoi197@gmail.com>
2024-04-30 23:33:27 -0600

    se avanzó en mostrar la portada cuando seleccionas cancion

--- Scrips/metadatos.js
+++ Scrips/metadatos.js
@@ -1,14 +1,16 @@
 console.log("hola mundo")
 
-function prueba_b(){
-    console.log ("me haz presionado")
-}
+
 
 class Metadatos {
-    constructor (nombre, artista, salida){
+    constructor (nombre, artista, salida, duracionM, duracionS, portada, audio){
         this.nombre = nombre
         this.artista = artista
         this.salida = salida
+        this.duracionM = duracionM
+        this.duracionS = duracionS
+        this.portada = portada
+        this.audio = audio
     }
     hablar(){
         return `en ${this.salida} salió la cancion ${this.nombre} del artista ${this.artista}`
@@ -17,19 +19,38 @@ class Metadatos {
 
 
 var cancion = [
-    new Metadatos("Enter sandman", "Metalica", 1991), 
-    new Metadatos ("Aca entre nos", "Vicente Fernades", 1992), 
-    new Metadatos ("Tu falta de querer", "Mon laferte", 2015)
+    new Metadatos("Enter sandman", "Metalica", 1991, 5, 31, "Media\\Cover\\The black album.jpg"), 
+    new Metadatos ("Aca entre nos", "Vicente Fernades", 1992, 3, 16, "Media\\Cover\\Vicente Fernandes - Que raro tiene.jpg"), 
+    new Metadatos ("Tu falta de querer", "Mon laferte", 2015, 4, 38, "Media\\Cover\\Mon Laferte - vol1.jpg"),
+    new Metadatos ("fly me to the moon", "Franck Sinatra", 1964, 2, 27, "Media\\Cover\\Nothing but the best.jpg")
 ]
 
-console.log(cancion[0].hablar())
+//console.log(cancion[0].hablar())
+
+function prueba_b(){
+    console.log(cancion[0].nombre);
+    let change = document.getElementById("Cover_display");
+    change.src = cancion[0].portada;
+
+}
 
 function bot1(){
-    alert (cancion[0].hablar())
+    console.log(cancion[0].nombre);
+    let change = document.getElementById("Cover_display");
+    change.src = cancion[0].portada;
 }
 function bot2(){
-    alert (cancion[1].hablar())
+    console.log(cancion[1].nombre);
+    let change = document.getElementById("Cover_display");
+    change.src = cancion[1].portada;
 }
 function bot3(){
-    alert (cancion[2].hablar())
+    console.log(cancion[2].nombre);
+    let change = document.getElementById("Cover_display");
+    change.src = cancion[2].portada;
+}
+function bot4(){
+    console.log(cancion[3].nombre);
+    let change = document.getElementById("Cover_display");
+    change.src = cancion[3].portada;
 }
